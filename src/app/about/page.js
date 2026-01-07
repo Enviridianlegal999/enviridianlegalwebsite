c8db8129a4c2a761a321e1c672cc3572654b0840
faraz-github <farazahmad.email@gmail.com>
2026-01-07 16:47:25 +0530

    Add aboutpage hero

---
+++ src/app/about/page.js
@@ -0,0 +1,11 @@
+import AboutPageHero from "@/components/pages/about/AboutPageHero";
+
+import styles from "@/styles/pages/AboutPage.module.css";
+
+export default function About() {
+  return (
+    <>
+      <AboutPageHero sectionID="main" styles={styles} />
+    </>
+  );
+}
---
+++ src/components/pages/about/AboutPageHero.jsx
@@ -0,0 +1,36 @@
+import { Grid, Stack } from "@mui/material";
+
+import Container from "@/components/layout/Container";
+
+const AboutPageHero = ({ sectionID, styles }) => {
+  return (
+    <section id={sectionID} className={styles.heroImageSection}>
+      {/* Video Overlay (optional - for better text contrast) */}
+      <div className={styles.heroOverlay}></div>
+
+      <Container>
+        <div className={styles.heroContent}>
+          <Grid
+            container
+            minHeight={"var(--half-viewport-height)"}
+            marginTop={{
+              xs: 4,
+              sm: 0,
+              lg: 0,
+            }}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <Grid size={12} maxWidth={"fit-content"}>
+              <h1 className={`${styles.heroTitle} gradientText`}>
+                About Enviridian Legal
+              </h1>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutPageHero;
