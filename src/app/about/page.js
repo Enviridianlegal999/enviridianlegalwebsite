e5db72c5846aa5db7cf95545506ffe291d191ffa
faraz-github <farazahmad.email@gmail.com>
2026-01-08 12:57:58 +0530

    Add aboutpage usp

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -2,6 +2,7 @@ import AboutPageHero from "@/components/pages/about/AboutPageHero";
 import AboutPageOverview from "@/components/pages/about/AboutPageOverview";
 import AboutPageStory from "@/components/pages/about/AboutPageStory";
 import AboutPageTeam from "@/components/pages/about/AboutPageTeam";
+import AboutPageUSP from "@/components/pages/about/AboutPageUSP";
 
 import styles from "@/styles/pages/AboutPage.module.css";
 
@@ -12,6 +13,7 @@ export default function About() {
       <AboutPageOverview sectionID="overview" styles={styles} />
       <AboutPageStory sectionID="our-story" styles={styles} />
       <AboutPageTeam sectionID="our-team" styles={styles} />
+      <AboutPageUSP sectionID="usp" styles={styles} />
     </>
   );
 }
---
+++ src/components/pages/about/AboutPageUSP.jsx
@@ -0,0 +1,112 @@
+import { Box, Grid, Stack } from "@mui/material";
+
+import Container from "@/components/layout/Container";
+
+import { FaHandHoldingHeart } from "react-icons/fa";
+import { FaRankingStar } from "react-icons/fa6";
+import { FaGlobeAsia } from "react-icons/fa";
+import { FaCheckCircle } from "react-icons/fa";
+
+const AboutPageUSP = ({ sectionID, styles }) => {
+  return (
+    <section id={sectionID} className={styles.uspSection}>
+      <Container>
+        <Stack spacing={4}>
+          <h2>What sets us apart?</h2>
+          <Grid container spacing={{ xs: 4, sm: 6, lg: 10 }}>
+            <Grid size={{ xs: 12, sm: 6, lg: 6 }}>
+              <Stack direction={"row"} alignItems={"center"} spacing={2}>
+                <Box
+                  sx={{
+                    border: "5px solid var(--secondary)",
+                    minHeight: { xs: 120, sm: 150, lg: 180 },
+                    minWidth: { xs: 120, sm: 150, lg: 180 },
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: "50%",
+                  }}
+                >
+                  <FaHandHoldingHeart fontSize={80} color="var(--secondary)" />
+                </Box>
+
+                <Stack spacing={2}>
+                  <h4>Tailored Solutions</h4>
+                  <p>Partner-led approach on every matter.</p>
+                </Stack>
+              </Stack>
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6, lg: 6 }}>
+              <Stack direction={"row"} alignItems={"center"} spacing={2}>
+                <Box
+                  sx={{
+                    border: "5px solid var(--secondary)",
+                    minHeight: { xs: 120, sm: 150, lg: 180 },
+                    minWidth: { xs: 120, sm: 150, lg: 180 },
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: "50%",
+                  }}
+                >
+                  <FaRankingStar fontSize={80} color="var(--secondary)" />
+                </Box>
+
+                <Stack spacing={2}>
+                  <h4>Expertise</h4>
+                  <p>Deep understanding of Delhi courts & regulators.</p>
+                </Stack>
+              </Stack>
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6, lg: 6 }}>
+              <Stack direction={"row"} alignItems={"center"} spacing={2}>
+                <Box
+                  sx={{
+                    border: "5px solid var(--secondary)",
+                    minHeight: { xs: 120, sm: 150, lg: 180 },
+                    minWidth: { xs: 120, sm: 150, lg: 180 },
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: "50%",
+                  }}
+                >
+                  <FaGlobeAsia fontSize={80} color="var(--secondary)" />
+                </Box>
+
+                <Stack spacing={2}>
+                  <h4>Global Reach</h4>
+                  <p>Modern technology for secure, efficient collaboration.</p>
+                </Stack>
+              </Stack>
+            </Grid>
+            <Grid size={{ xs: 12, sm: 6, lg: 6 }}>
+              <Stack direction={"row"} alignItems={"center"} spacing={2}>
+                <Box
+                  sx={{
+                    border: "5px solid var(--secondary)",
+                    minHeight: { xs: 120, sm: 150, lg: 180 },
+                    minWidth: { xs: 120, sm: 150, lg: 180 },
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    borderRadius: "50%",
+                  }}
+                >
+                  <FaCheckCircle fontSize={80} color="var(--secondary)" />
+                </Box>
+
+                <Stack spacing={2}>
+                  <h4>Trust</h4>
+                  <p>Fixed-fee options for predictable budgeting.</p>
+                </Stack>
+              </Stack>
+            </Grid>
+          </Grid>
+        </Stack>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutPageUSP;
