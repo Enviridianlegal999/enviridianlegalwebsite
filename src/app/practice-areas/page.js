91fa9f3afa72b44c25ac18c8b63f1b8c5ca7526a
faraz-github <farazahmad.email@gmail.com>
2026-01-08 20:27:33 +0530

    Add practicearea hero

---
+++ src/app/practice-areas/page.js
@@ -0,0 +1,11 @@
+import PracticeAreasPageHero from "@/components/pages/practice-areas/PracticeAreasPageHero";
+
+import styles from "@/styles/pages/PracticeAreasPage.module.css";
+
+export default function PracticeAreasPage() {
+  return (
+    <>
+      <PracticeAreasPageHero sectionID="main" styles={styles} />
+    </>
+  );
+}
---
+++ src/components/pages/practice-areas/PracticeAreasPageHero.jsx
@@ -0,0 +1,35 @@
+import Container from "@/components/layout/Container";
+import { Grid } from "@mui/material";
+
+const PracticeAreasPageHero = ({ sectionID, styles }) => {
+  return (
+    <section id={sectionID} className={styles.heroImageSection}>
+      {/* Video Overlay (optional - for better text contrast) */}
+      <div className={styles.heroOverlay}></div>
+
+      <Container>
+        <div className={styles.heroContent}>
+          <Grid
+            container
+            minHeight={"var(--half-viewport-height)"}
+            marginTop={{
+              xs: 4,
+              sm: 0,
+              lg: 0,
+            }}
+            alignItems={"center"}
+            justifyContent={"center"}
+          >
+            <Grid size={12} maxWidth={"fit-content"}>
+              <h1 className={`${styles.heroTitle} gradientText`}>
+                Our Practice Areas
+              </h1>
+            </Grid>
+          </Grid>
+        </div>
+      </Container>
+    </section>
+  );
+};
+
+export default PracticeAreasPageHero;
