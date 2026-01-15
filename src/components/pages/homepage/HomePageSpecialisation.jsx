81ea6b18ba2ebc732250b21df8ef26fe89cc15ff
faraz-github <farazahmad.email@gmail.com>
2026-01-15 19:54:08 +0530

    Homepage specialization complete

--- src/components/pages/homepage/HomePageSpecialisation.jsx
+++ src/components/pages/homepage/HomePageSpecialisation.jsx
@@ -1,5 +1,7 @@
 import { Box, Grid, Stack } from "@mui/material";
 
+import * as motion from "motion/react-client";
+
 import Container from "@/components/layout/Container";
 import SectionBookMark from "@/components/layout/SectionBookMark";
 
@@ -10,116 +12,138 @@ const HomePageSpecialisation = ({ sectionID, styles }) => {
       <Container>
         <SectionBookMark title={"Practice Areas"} />
         <Stack spacing={2} my={4}>
-          <h2>Our Specialisation</h2>
-          <Stack
-            justifyContent={"space-between"}
-            direction={{ xs: "column", sm: "row" }}
-            spacing={2}
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4 }}
+            viewport={{ once: true }}
           >
-            <p>
-              With focus on public law jurisprudence and selective IP matters,
-              we ensure detailed, issue-oriented representation - not volume.
-            </p>
-            <p>
-              Based in Delhi NCR - we handle matters across High Courts,
-              Tribunals & Supreme Court.
-            </p>
-          </Stack>
+            <h2>Our Specialisation</h2>
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.4, delay: 0.2 }}
+            viewport={{ once: true }}
+          >
+            <Stack
+              justifyContent={"space-between"}
+              direction={{ xs: "column", sm: "row" }}
+              spacing={2}
+            >
+              <p>
+                With focus on public law jurisprudence and selective IP matters,
+                we ensure detailed, issue-oriented representation - not volume.
+              </p>
+              <p>
+                Based in Delhi NCR - we handle matters across High Courts,
+                Tribunals & Supreme Court.
+              </p>
+            </Stack>
+          </motion.div>
         </Stack>
-        <Grid container spacing={2}>
-          <Grid
-            size={{ xs: 12, sm: 6, lg: 4 }}
-            className={styles.specialisationCard}
-            position={"relative"}
-          >
-            <Box
-              sx={{
-                height: "fit-content",
-                width: "100%",
-                background: "var(--black-overlay)",
-                position: "absolute",
-                bottom: 0,
-                borderRadius: "var(--high-rounded)",
-                padding: "20px",
-              }}
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.4, delay: 0.4 }}
+          viewport={{ once: true }}
+        >
+          <Grid container spacing={2}>
+            <Grid
+              size={{ xs: 12, sm: 6, lg: 4 }}
+              className={styles.specialisationCard}
+              position={"relative"}
             >
-              <Stack spacing={2}>
-                <span className={styles.specialisationCardTitle}>
-                  CONSTITUTIONAL LAW
-                </span>
-                <span className={styles.specialisationCardSubTitle}>
-                  Writs, Fundamental Rights & Public Law
-                </span>
-                <p className={styles.specialisationCardDescription}>
-                  Challenging legislation and executive action, defending
-                  fundamental rights, regulatory & policy disputes, public
-                  interest litigation.
-                </p>
-              </Stack>
-            </Box>
-          </Grid>
-          <Grid
-            size={{ xs: 12, sm: 6, lg: 4 }}
-            className={styles.specialisationCard}
-            position={"relative"}
-          >
-            <Box
-              sx={{
-                height: "fit-content",
-                width: "100%",
-                background: "var(--black-overlay)",
-                position: "absolute",
-                bottom: 0,
-                borderRadius: "var(--high-rounded)",
-                padding: "20px",
-              }}
+              <Box
+                sx={{
+                  height: "fit-content",
+                  width: "100%",
+                  background: "var(--black-overlay)",
+                  position: "absolute",
+                  bottom: 0,
+                  borderRadius: "var(--high-rounded)",
+                  padding: "20px",
+                }}
+              >
+                <Stack spacing={2}>
+                  <span className={styles.specialisationCardTitle}>
+                    CONSTITUTIONAL LAW
+                  </span>
+                  <span className={styles.specialisationCardSubTitle}>
+                    Writs, Fundamental Rights & Public Law
+                  </span>
+                  <p className={styles.specialisationCardDescription}>
+                    Challenging legislation and executive action, defending
+                    fundamental rights, regulatory & policy disputes, public
+                    interest litigation.
+                  </p>
+                </Stack>
+              </Box>
+            </Grid>
+            <Grid
+              size={{ xs: 12, sm: 6, lg: 4 }}
+              className={styles.specialisationCard}
+              position={"relative"}
             >
-              <Stack spacing={2}>
-                <span className={styles.specialisationCardTitle}>
-                  SERVICE & ADMINISTRATIVE LAW
-                </span>
-                <span className={styles.specialisationCardSubTitle}>
-                  Service Disputes, Employment Tribunals, Seniority & Promotions
-                </span>
-                <p className={styles.specialisationCardDescription}>
-                  Representation in disciplinary proceedings, tribunal cases,
-                  service rule matters, and administrative jurisprudence.
-                </p>
-              </Stack>
-            </Box>
-          </Grid>
-          <Grid
-            size={{ xs: 12, sm: 12, lg: 4 }}
-            className={styles.specialisationCard}
-            position={"relative"}
-          >
-            <Box
-              sx={{
-                height: "fit-content",
-                width: "100%",
-                background: "var(--black-overlay)",
-                position: "absolute",
-                bottom: 0,
-                borderRadius: "var(--high-rounded)",
-                padding: "20px",
-              }}
+              <Box
+                sx={{
+                  height: "fit-content",
+                  width: "100%",
+                  background: "var(--black-overlay)",
+                  position: "absolute",
+                  bottom: 0,
+                  borderRadius: "var(--high-rounded)",
+                  padding: "20px",
+                }}
+              >
+                <Stack spacing={2}>
+                  <span className={styles.specialisationCardTitle}>
+                    SERVICE & ADMINISTRATIVE LAW
+                  </span>
+                  <span className={styles.specialisationCardSubTitle}>
+                    Service Disputes, Employment Tribunals, Seniority &
+                    Promotions
+                  </span>
+                  <p className={styles.specialisationCardDescription}>
+                    Representation in disciplinary proceedings, tribunal cases,
+                    service rule matters, and administrative jurisprudence.
+                  </p>
+                </Stack>
+              </Box>
+            </Grid>
+            <Grid
+              size={{ xs: 12, sm: 12, lg: 4 }}
+              className={styles.specialisationCard}
+              position={"relative"}
             >
-              <Stack spacing={2}>
-                <span className={styles.specialisationCardTitle}>
-                  IP & REGULATORY INTERSECTION
-                </span>
-                <span className={styles.specialisationCardSubTitle}>
-                  IP Disputes with Public Law Elements
-                </span>
-                <p className={styles.specialisationCardDescription}>
-                  Trademark & copyright disputes, regulatory compliance,
-                  licensing issues, public interest contentions, policy-oriented
-                  IP work.
-                </p>
-              </Stack>
-            </Box>
+              <Box
+                sx={{
+                  height: "fit-content",
+                  width: "100%",
+                  background: "var(--black-overlay)",
+                  position: "absolute",
+                  bottom: 0,
+                  borderRadius: "var(--high-rounded)",
+                  padding: "20px",
+                }}
+              >
+                <Stack spacing={2}>
+                  <span className={styles.specialisationCardTitle}>
+                    IP & REGULATORY INTERSECTION
+                  </span>
+                  <span className={styles.specialisationCardSubTitle}>
+                    IP Disputes with Public Law Elements
+                  </span>
+                  <p className={styles.specialisationCardDescription}>
+                    Trademark & copyright disputes, regulatory compliance,
+                    licensing issues, public interest contentions,
+                    policy-oriented IP work.
+                  </p>
+                </Stack>
+              </Box>
+            </Grid>
           </Grid>
-        </Grid>
+        </motion.div>
       </Container>
     </section>
   );
